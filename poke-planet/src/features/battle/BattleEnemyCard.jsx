26b39abd86a6c4c9a6685518047ba83c610a340d
Hyliangoat <reignoflife@gmail.com>
2026-07-25 17:19:40 -0400

    Created tests

--- poke-planet/src/features/battle/BattleEnemyCard.jsx
+++ poke-planet/src/features/battle/BattleEnemyCard.jsx
@@ -1,9 +1,18 @@
+function formatArchetype(archetype) {
+  if (!archetype) {
+    return 'Balanced';
+  }
+  return archetype.charAt(0).toUpperCase() + archetype.slice(1);
+}
+
 export default function BattleEnemyCard({ enemy, isBoss }) {
   const size = isBoss ? 200 : 100;
 
   return (
     <div>
       <p>{enemy.name}</p>
+      <p>{formatArchetype(enemy.archetype)} opponent</p>
+      {!isBoss && <p>Threat score: {enemy.threatScore}</p>}
       <img
         src={enemy.avatar}
         height={size}
